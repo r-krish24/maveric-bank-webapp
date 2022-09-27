92036f10b1c432b5045fc2a79b9d1069f09b2db5
Ram Krishnan <ramkrishnane@maveric-systems.com>
2022-09-27 16:08:59 +0530

    Added example modal

--- src/app/components/accounts/accounts.component.ts
+++ src/app/components/accounts/accounts.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { AppComponent } from 'src/app/app.component';
 import { Account } from 'src/app/models/account';
 import { AccountService } from 'src/app/services/account.service';
+declare var window: any;
 
 @Component({
   selector: 'app-accounts',
@@ -12,10 +13,15 @@ import { AccountService } from 'src/app/services/account.service';
 export class AccountsComponent implements OnInit {
   account:Account[];
   _id:String;
+  formModal: any;
+
   constructor(private route:ActivatedRoute, private router:Router,
     private accountService:AccountService,private app:AppComponent) { }
-
+    display = "none";
   ngOnInit(): void {
+    this.formModal = new window.bootstrap.Modal(
+      document.getElementById('myModal')
+    );
     this.app.loginshow();
     this._id=this.route.snapshot.params['id'];
     this.accountService.getAccount(this._id).subscribe(data=>{
@@ -25,6 +31,17 @@ export class AccountsComponent implements OnInit {
   accountDetails(aid:String){
     this._id=this.route.snapshot.params['id'];
     this.router.navigate(['account-details',this._id,aid]);
+  }
+  openFormModal() {
+    console.log("Inside open");
+    this.display = "block";
+    this.formModal.show();
+  }
+  saveSomeThing() {
+    // confirm or save something
+    console.log("Inside close");
+    this.display = "none";
+    this.formModal.hide();
   }
 
 }
